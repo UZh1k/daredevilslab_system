4440c05da292ed57ff9e0e6a5409685783d47795
UZh1k <danyanygin4@gmail.com>
2020-11-02 19:47:46 +0300

    commit-test3

--- daredevilslabs_system/settings.py
+++ daredevilslabs_system/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'daredevilslabs_system.qrreader',
+    'qrreader',
     'qr_code',
 ]
 
